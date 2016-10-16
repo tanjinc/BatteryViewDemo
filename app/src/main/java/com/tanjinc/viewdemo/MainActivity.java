0f2db36731b9c2a5a3817c798484b4e9580023eb
tanjinc <tanjinc@126.com>
2016-10-16 15:32:57 +0800

    first commit

---
+++ app/src/main/java/com/tanjinc/viewdemo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tanjinc.viewdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
